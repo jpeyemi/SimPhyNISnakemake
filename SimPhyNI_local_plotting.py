185639a34bd646c615b73a71c00577d3e6d5d492
iobal <iobal@eofe7.mit.edu>
2025-05-22 12:05:52 -0400

    local plotting script

--- SimPhyNI_local_plotting.py
+++ SimPhyNI_local_plotting.py
@@ -1,4 +1,7 @@
 import pickle
+import sys
+sys.path.insert(0,"SimPhyNI/Simulation")
+sys.path.insert(0,"SimPhyNI")
 from tree_simulator import TreeSimulator
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,19 +9,20 @@ import pandas as pd
 import numpy as np
 import sys
 
+simphyni_object = 'sim_kde.pkl'
 # Load simulation
-with open("sim_kde.pkl", "rb") as f:
+with open(simphyni_object, "rb") as f:
     Sim: TreeSimulator = pickle.load(f)
 
-# Plot Results
-Sim.plot_results(correction = 'fdr_tsbky', figure_size=10, prevalence_range=[0.05, 0.95])
+# Plot Results Heatmap (All against All)
+Sim.plot_results(correction = 'fdr_bhc', figure_size=10, prevalence_range=[0.05, 0.95])
 
 # Plot Effect Sizes
-Sim.plot_effect_size(correction = 'fdr_tsbky', prevalence_range=[0.05, 0.95])
+Sim.plot_effect_size(correction = 'fdr_bhc', prevalence_range=[0.05, 0.95])
 
 # Top Significant Results
 res = Sim.get_top_results(
-    correction='fdr_tsbky',
+    correction='fdr_bhc',
     prevalence_range=[0.05, 0.95],
     top=50,
     direction=0,
